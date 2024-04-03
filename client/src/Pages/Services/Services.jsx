a51a8dd3a7ef83bd3ac5f9cf1391c98a70d3be6b
ankush <kumarankush13701@gmail.com>
2024-04-03 16:50:05 +0530

    lorem added

--- client/src/Pages/Services/Services.jsx
+++ client/src/Pages/Services/Services.jsx
@@ -5,6 +5,7 @@ const Services = () => {
   return (
     <div className='px-[2%] py-[10rem]'>
       <div>Services</div>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus veniam odio corporis adipisci ipsam placeat ab mollitia temporibus in nisi! </h1>
       <ScrollingContact/>
     </div>
   );
